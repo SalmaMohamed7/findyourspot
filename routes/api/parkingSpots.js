0a6369211f2c2f5d9c00cb303294a16d3857bfb4
MayarMahmoud <Mayar.Khalifa@student.guc.edu.eg>
2019-12-05 13:30:41 +0200

    update parking spots

--- routes/api/parkingSpots.js
+++ routes/api/parkingSpots.js
@@ -35,5 +35,27 @@ router.post('/', async (req,res) => {
     const parkingSpots = await parkingSpot.find()
     res.json({data: parkingSpots})
 })
+router.put('/:id', async (req, res) => {
+    
+    const isValidated = validator.updateValidation(req.body)
+    console.log("in put parking spots")
+    if (isValidated.error)
+    {
+        console.log(isValidated.error.details[0].message)
+        return res.status(400).send({ error: isValidated.error.details[0].message });
+    }
+    
+    else{
+        console.log("updating parking spots..")
+        console.log(req.body)
+        console.log(req.params.id)
+
+        console.log("here")
+    await parkingSpot.findByIdAndUpdate(req.params.id, req.body)
+    .exec()
+    .then(r => {return res.redirect(303, `/api/parkingSpot/${req.params.id}`) })
+    .catch(err => {console.log(err); return res.status(400).send(`Sorry, couldn't update a parkingSpot with that id !`) })
+    }
+})
 
 module.exports = router
